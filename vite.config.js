0363533a778cd03c57efa7cddb447b9d33dd8180
NataliaStupka <84662491+NataliaStupka@users.noreply.github.com>
2024-11-02 00:12:34 +0200

    fix

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,19 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    sourcemap: true,
+  },
 });
 
-// //по конспекту, треба замінити на
-// import { defineConfig } from 'vite';
-// import react from '@vitejs/plugin-react-swc';
+// // ===== було =======
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react-swc'
 
-// // https://vitejs.dev/config/
+// // https://vite.dev/config/
 // export default defineConfig({
 //   plugins: [react()],
-//   build: {
-//     sourcemap: true,
-//   },
-// });
+// })

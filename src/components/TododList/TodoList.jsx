638857ee7fb8580ccf6561212d2d19030ae2ef21
NataliaStupka <84662491+NataliaStupka@users.noreply.github.com>
2024-11-05 12:44:57 +0200

    localStorage

--- src/components/TododList/TodoList.jsx
+++ src/components/TododList/TodoList.jsx
@@ -1,11 +1,15 @@
-import { useState } from 'react';
+//useEffect(() => {що робимо, при змінні того за чим слідкуємо}, [за чим слідкуємо])
+import { useState, useEffect } from 'react';
 import s from './Todo.module.css';
 import { nanoid } from 'nanoid'; //генерування id
 import TodoItem from './TodoItem'; //компоненнт
 import todosData from '../../assets/todosData.json'; //масив об'єктів справ
 
 const TodoList = () => {
-  const [todos, setTodos] = useState(todosData);
+  const [todos, setTodos] = useState(
+    // (*2)якщо в localStorage є 'todos' рендеремо його, як ні то todosData
+    () => JSON.parse(localStorage.getItem('todos')) ?? todosData
+  );
   const [newValue, setNewValue] = useState('');
 
   //console.log('todos', todos);
@@ -16,6 +20,11 @@ const TodoList = () => {
     //поверне тільки ті, що не = вибраному id
     setTodos(prev => prev.filter(item => item.id !== id));
   };
+  //(*1) зберігаємо в localStorage
+  useEffect(() => {
+    localStorage.setItem('todos', JSON.stringify(todos));
+  }, [todos]);
+
   //додати ========
   const handleAddTodo = () => {
     console.log('Додаємо замітку');

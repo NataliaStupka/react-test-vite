5e382a0c76c150dbe2d223d5e793320f9a8b2913
NataliaStupka <84662491+NataliaStupka@users.noreply.github.com>
2024-11-21 17:02:22 +0200

    Link

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,15 @@
 import s from './Header.module.css';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
   return (
     <div>
       <header className={s.header}>
         <h3>Routing</h3>
+        <nav className={s.nav}>
+          <Link to="/">Home</Link>
+          <Link to="/about">About</Link>
+        </nav>
       </header>
     </div>
   );

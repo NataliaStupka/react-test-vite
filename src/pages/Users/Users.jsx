7d4c43ace2b8c326590dc1fdd5c5e9d74fbb2dc6
NataliaStupka <84662491+NataliaStupka@users.noreply.github.com>
2024-11-22 14:00:51 +0200

    SearchParams

--- src/pages/Users/Users.jsx
+++ src/pages/Users/Users.jsx
@@ -1,11 +1,11 @@
 import { useEffect, useState } from 'react';
 import { fetchUsers } from '../../services/api';
-import { Link } from 'react-router-dom';
+import { Link, useSearchParams } from 'react-router-dom';
 import SearchBar from '../../components/SearchBar/SearchBar';
 
 const Users = () => {
   const [users, setUsers] = useState([]);
-  const [query, setQuery] = useState(''); //для фільтрування тільки тих, що входять у список співпадінь з нашим словом
+  const [searchParams, setSearchParams] = useSearchParams();
 
   useEffect(() => {
     const getData = async () => {
@@ -15,12 +15,19 @@ const Users = () => {
     getData(); //викликаємо
   }, []);
 
+  //віддай query, якщо нічого не повернеться то додай порожню строку
+  const query = searchParams.get('query') ?? '';
+
   const filteredData = users.filter(user =>
     user.firstName.toLowerCase().includes(query.toLowerCase())
   );
 
+  //запише в url значення query і те, що прийде з форми set, а зчитаємо через get
   const handleSetQurey = newValue => {
-    setQuery(newValue);
+    //Встановлює в url те, що написали в set: query, значення
+    searchParams.set('query', newValue);
+
+    setSearchParams(searchParams);
   };
 
   return (
